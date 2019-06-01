3c17c74c1642b9b5259c25019312018738c89f9b
Student <magnusarbuthnott@gmail.com>
2019-06-01 20:55:47 +0100

    Added an API to display the temperature in the current city

--- interface.js
+++ interface.js
@@ -29,6 +29,24 @@ $(document).ready(function() {
     refreshPowerSavingMode();
   });
 
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+  $('#current-temperature').text(data.main.temp);
+  })
+
+  $('#current-city').change(function(event) {
+    var city = $('#current-city').val();
+    displayWeather(city);
+  })
+
+  function displayWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+    var units = '&units=metric';
+    $.get(url + token + units, function(data) {
+      $('#current-temperature').text(data.main.temp);
+    })
+  }
+
   function refreshTemperature() {
     $("#temperature").text(thermostat.getCurrentTemperature() + " °C");
     $("#temperature").attr('class', thermostat.energyUsage());
